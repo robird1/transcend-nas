b5b68c79f216aa755e23b06ad8e27e596292ae6d
steve su <steve_su@transcend-info.com>
2017-02-08 13:48:26 +0800

    1. fix OOM issue 2. code refactoring

--- app/src/main/java/com/transcend/nas/management/externalstorage/ViewerPagerAdapterSD.java
+++ app/src/main/java/com/transcend/nas/management/externalstorage/ViewerPagerAdapterSD.java
@@ -1,17 +1,18 @@
 package com.transcend.nas.management.externalstorage;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.support.v4.view.PagerAdapter;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.transcend.nas.R;
 
 import java.util.ArrayList;
-
-import uk.co.senab.photoview.PhotoView;
 
 /**
  * Created by steve_su on 2017/1/4.
@@ -25,10 +26,10 @@ public class ViewerPagerAdapterSD extends PagerAdapter {
 
     public ViewerPagerAdapterSD(Context context) {
         mContext = context;
-        mList = new ArrayList<Integer>();
+        mList = new ArrayList<>();
         mList.add(R.drawable.guigeimage_sd1);
         mList.add(R.drawable.guigeimage_sd2);
-        mList.add((R.drawable.guigeimage_sd3));
+        mList.add(R.drawable.guigeimage_sd3);
     }
 
     @Override
@@ -43,12 +44,17 @@ public class ViewerPagerAdapterSD extends PagerAdapter {
 
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
-        int id = mList.get(position);
-        PhotoView pv = new PhotoView(mContext);
-        pv.setDrawingCacheEnabled(false);
-        pv.setImageResource(id);
-        container.addView(pv);
-        return pv;
+        String uri = Uri.parse("drawable://" + mList.get(position)).toString();
+        ImageView iv = new ImageView(mContext);
+        DisplayImageOptions options = new DisplayImageOptions.Builder()
+                .bitmapConfig(Bitmap.Config.RGB_565)
+                .cacheInMemory(true)
+                .cacheOnDisk(false)
+                .build();
+        ImageLoader.getInstance().displayImage(uri, iv, options);
+
+        container.addView(iv);
+        return iv;
     }
 
     @Override
@@ -58,12 +64,6 @@ public class ViewerPagerAdapterSD extends PagerAdapter {
             ImageLoader.getInstance().cancelDisplayTask(iv);
             container.removeView(iv);
         }
-    }
-
-
-    public void removeView(int index) {
-        mList.remove(index);
-        notifyDataSetChanged();
     }
 
     @Override
